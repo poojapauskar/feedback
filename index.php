6e3496571f88c24a9cacf09ee1f084951c9a3c5b
poojapauskar <git.poojapauskar@gmail.com>
2016-03-25 16:57:05 +0530

    2 pages created

--- index.php
+++ index.php
@@ -16,15 +16,46 @@ function show_wait_msg ()
 }
 
 
+
 </script>
 <body onload="hide_wait_msg()">
 
 
 <?php 
 
-/*echo $_POST['feedback'];*/
+/*if (!isset($_COOKIE['FormSubmitted']))
+{
+    setcookie('FormSubmitted', '0');
+}
+if ($_COOKIE['FormSubmitted'] == 0)
+{
+    setcookie('FormSubmitted', '1');
+}
+if ($_COOKIE['FormSubmitted'] == 1)
+{
+    setcookie('FormSubmitted', '2');
+} 
+if ($_COOKIE['FormSubmitted'] == 2)
+{
+    setcookie('FormSubmitted', '3');
+} 
+if ($_COOKIE['FormSubmitted'] == 3)
+{
+    setcookie('FormSubmitted', '4');
+}
+if ($_COOKIE['FormSubmitted'] == 4)
+{
+    setcookie('FormSubmitted', '5');
+}  
+if ($_COOKIE['FormSubmitted'] == 5)
+{
+    die('Thank you for your feedback!');
+} */
 
 if($_POST['feedback'] != ''){
+
+
+
     $url = 'https://feedback-list.herokuapp.com/set_feedback/';
     $data = array('value' => $_POST['feedback']);
     // use key 'http' even if you send the request to https://...
@@ -39,14 +70,14 @@ if($_POST['feedback'] != ''){
     $result = file_get_contents($url, false, $context);
     /*echo $result8;*/
     $arr = json_decode($result,true);
-    $message="Thank You for your feedback";
+    $message="Thank You for your feedback!";
     
 }?>
 
 
 
 
-<div id="loadingPleaseWait"><div>Loading, please wait...</div></div>
+<div id="loadingPleaseWait"><h6>Loading, please wait...</h6></div>
 
 <h5><?php echo $message; ?><h5>
 
@@ -54,13 +85,63 @@ if($_POST['feedback'] != ''){
 <h4>Feedback Form</h4>
 
 <form action="" method="post" id="myform" name="form1">
-   <input type="hidden" value="0" name="feedback"></input>
-   <button onclick="show_wait_msg()" class="btn1" id="btn" type="submit">Bad</button>
+   <input type="hidden" value="1" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn1" class="btn btn-md round">this is boring</button>
 </form>
 
 <form action="" method="post" id="myform" name="form2">
-   <input type="hidden" value="1" name="feedback"></input>
-   <button onclick="show_wait_msg()" class="btn2" id="btn" type="submit">Good</button>
+   <input type="hidden" value="2" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn2" class="btn btn-md round">need to know more...</button>
+</form>
+
+<form action="" method="post" id="myform" name="form3">
+   <input type="hidden" value="3" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn3" class="btn btn-md round">i feel sleepy</button>
+</form>
+
+<form action="" method="post" id="myform" name="form4">
+   <input type="hidden" value="4" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn4" class="btn btn-md round">please slow down</button>
+</form>
+
+<form action="" method="post" id="myform" name="form5">
+   <input type="hidden" value="5" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn5" class="btn btn-md round">this is interesting</button>
+</form>
+
+<form action="" method="post" id="myform" name="form6">
+   <input type="hidden" value="6" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn6" class="btn btn-md round">i'm saturated</button>
+</form>
+
+<form action="" method="post" id="myform" name="form7">
+   <input type="hidden" value="7" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn7" class="btn btn-md round">i'm lovin it</button>
+</form>
+
+<form action="" method="post" id="myform" name="form8">
+   <input type="hidden" value="8" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn8" class="btn btn-md round">i need a chai break</button>
+</form>
+
+<form action="" method="post" id="myform" name="form9">
+   <input type="hidden" value="9" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn9" class="btn btn-md round">lost the track</button>
+</form>
+
+<form action="" method="post" id="myform" name="form10">
+   <input type="hidden" value="10" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn10" class="btn btn-md round">this is informative</button>
+</form>
+
+<form action="" method="post" id="myform" name="form11">
+   <input type="hidden" value="11" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn11" class="btn btn-md round">best thing in the WORLD</button>
+</form>
+
+<form action="" method="post" id="myform" name="form12">
+   <input type="hidden" value="12" name="feedback"></input>
+   <button onclick="show_wait_msg()" type="submit" id="btn12" class="btn btn-md round">why am i here???</button>
 </form>
 
 
--- results.php
+++ results.php
@@ -1,16 +1,142 @@
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="results.css">
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Animated HTML/CSS/jQuery Skills Graph</title>
+    
+    
+    
+    <link rel='stylesheet prefetch' href='bootstrap1.css'>
 
-<style>
-</style>
-<body>
+        <link rel="stylesheet" href="results.css">
 
-<script type="text/javascript">
-  setTimeout(function(){
+ 
+    
+    
+  </head>
+
+  <body>
+ <script type="text/javascript">
+/*  setTimeout(function(){
    window.location.reload(1);
-}, 1000);
-</script>
+}, 1000);*/
+</script>  
+
+      <div id="resumeProficiencies">
+    
+       <div id="resumeProficienciesTop" class="resumeProficienciesTop1">this is boring</div><!--END resumeProficienciesTop-->
+
+       <div id="resumeProficienciesBottom">
+        <div class="progress">
+          <div class="adobe progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+          </div>
+        </div>
+      </div><!--END resumeProficienciesBottom-->
+
+      <div id="resumeProficienciesTop">need to know more...</div><!--END resumeProficienciesTop-->
+     
+     <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="html progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+     </div><!--END resumeProficienciesBottom-->   
+
+     <div id="resumeProficienciesTop">i feel sleepy</div><!--END resumeProficienciesTop-->
+
+     <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="css progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+    </div><!--END resumeProficienciesBottom-->   
+
+    <div id="resumeProficienciesTop">please slow down</div><!-- END resumeProficienciesTop -->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="jquery progress-bar progress-bar-info" role="progressbar" style="width: 0%"></div>
+      </div>
+    </div><!--END resumeProficienciesBottom-->
+
+    <div id="resumeProficienciesTop">this is interesting</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="javascript progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+    </div><!--END resumeProficienciesBottom-->
+
+    <div id="resumeProficienciesTop">i'm saturated</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="bootstrap progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+    </div><!--END resumeProficienciesBottom-->
+
+    <div id="resumeProficienciesTop">i'm lovin it</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="wordpress progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+    </div><!--END resumeProficienciesBottom-->   
+
+    <div id="resumeProficienciesTop">i need a chai break</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="ui progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+    </div><!-- ENDresumeProficienciesBottom-->   
+
+    <div id="resumeProficienciesTop">lost the track</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="ux progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+        </div>
+      </div>
+    </div><!--END resumeProficienciesBottom-->   
+
+    <div id="resumeProficienciesTop">this is informative</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="rwd progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+         <span class="sr-only"></span>
+       </div>
+     </div>
+    </div><!--END resumeProficienciesBottom-->
+
+    <div id="resumeProficienciesTop">best thing in the WORLD</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="mobile progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+         <span class="sr-only"></span>
+       </div>
+     </div>
+    </div><!--END resumeProficienciesBottom-->
+
+    <div id="resumeProficienciesTop">why am i here???</div><!--END resumeProficienciesTop-->
+
+    <div id="resumeProficienciesBottom">
+      <div class="progress">
+        <div class="rapidproto progress-bar progress-bar-info" role="progressbar" style="width: 0%">
+         <span class="sr-only"></span>
+       </div>
+     </div>
+    </div><!--END resumeProficienciesBottom-->   
+</div>
+    <script src='jquery1.js'></script>
+<script src='jquery2.js'></script>
+
 
 <?php
 
@@ -31,26 +157,25 @@ $arr_check = json_decode($output_check,true);
 ?>
 
 
+<script type="text/javascript">
+$(document).ready(function() {
+  $('.adobe').css('width', "<?php echo $arr_check[0]['this is boring'];echo '%'; ?>");
+  $('.html').css('width',"<?php echo $arr_check[0]['need to know more...'];echo '%'; ?>");
+  $('.css').css('width', "<?php echo $arr_check[0]['i feel sleepy'];echo '%'; ?>");
+  $('.lesssass').css('width', "<?php echo $arr_check[0]['please slow down'];echo '%'; ?>");
+  $('.jquery').css('width', "<?php echo $arr_check[0]['this is interesting'];echo '%'; ?>");
+  $('.javascript').css('width',"<?php echo $arr_check[0]['im saturated'];echo '%'; ?>");
+  $('.bootstrap').css('width', "<?php echo $arr_check[0]['im lovin it'];echo '%'; ?>");
+  $('.wordpress').css('width', "<?php echo $arr_check[0]['i need a chai break'];echo '%'; ?>");
+  $('.ui').css('width', "<?php echo $arr_check[0]['lost the track'];echo '%'; ?>");
+  $('.ux').css('width', "<?php echo $arr_check[0]['this is informative'];echo '%'; ?>");
+  $('.rwd').css('width', "<?php echo $arr_check[0]['best thing in the WORLD'];echo '%'; ?>");
+  $('.mobile').css('width', "<?php echo $arr_check[0]['why am i here???'];echo '%'; ?>");
+});
+</script>
 
-
-<h4>Results</h4>
-
-<table>
-  <tr>
-    <th>Good</th>
-    <th>Bad</th>
-  </tr>
-
-  <tr>
-    <td><?php echo $arr_check[0]['good'] ?></td>
-    <td><?php echo $arr_check[0]['bad']; ?></td> 
-  </tr>
-  
-</table>
-
-
-</body>
+    
+    
+    
+  </body>
 </html>
-
-
-
