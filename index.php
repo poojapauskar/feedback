c98f7643e01664cb02b2c9e49e727e0041359003
poojapauskar <git.poojapauskar@gmail.com>
2016-03-28 12:57:35 +0530

    gif images added

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="feedback_form.css">
+
+<link rel="stylesheet" href="bootstrap1.css">
+  <script src="jquery2.js"></script>
+  <script src="bootstrap.js"></script>
+
+
+<title>Feedback</title>
 </head>
 <script type="text/javascript">
 
@@ -23,7 +30,7 @@ function show_wait_msg ()
 
 <?php 
 
-/*if (!isset($_COOKIE['FormSubmitted']))
+if (!isset($_COOKIE['FormSubmitted']))
 {
     setcookie('FormSubmitted', '0');
 }
@@ -50,7 +57,7 @@ if ($_COOKIE['FormSubmitted'] == 4)
 if ($_COOKIE['FormSubmitted'] == 5)
 {
     die('Thank you for your feedback!');
-} */
+} 
 
 if($_POST['feedback'] != ''){
 
@@ -77,73 +84,87 @@ if($_POST['feedback'] != ''){
 
 
 
-<div id="loadingPleaseWait"><h6>Loading, please wait...</h6></div>
 
-<h5><?php echo $message; ?><h5>
+
+<!-- <h5><?php echo $message; ?><h5> -->
 
   
-<h4>Feedback Form</h4>
+<!-- <h4>Feedback Form</h4> -->
 
-<form action="" method="post" id="myform" name="form1">
-   <input type="hidden" value="1" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn1" class="btn btn-md round">this is boring</button>
-</form>
 
-<form action="" method="post" id="myform" name="form2">
-   <input type="hidden" value="2" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn2" class="btn btn-md round">need to know more...</button>
-</form>
+<div id="loadingPleaseWait"><h6>Loading, please wait...</h6></div>
+      <form action="" style="margin-top:2%" method="post" id="myform" name="form1">
+         <input type="hidden" value="1" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit"><img src="monkey.gif"></img>this is boring</button> -->
+         <input  onclick="show_wait_msg()" type="image" src="images/boring.png" border="0" alt="Submit" />
+      </form>
+    
+      <form action="" method="post" id="myform" name="form2">
+         <input type="hidden" value="2" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn2" class="btn btn-md round">need to know more...</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/know_more.png" border="0" alt="Submit" />
+      </form>
+    
+      <form action="" method="post" id="myform" name="form3">
+         <input type="hidden" value="3" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn3" class="btn btn-md round">i feel sleepy</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/sleepy.png" border="0" alt="Submit" />
+      </form>
+    
+      <form action="" method="post" id="myform" name="form4">
+         <input type="hidden" value="4" name="feedback"></input>
+        <!--  <button onclick="show_wait_msg()" type="submit" id="btn4" class="btn btn-md round">please slow down</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/slow-down.png" border="0" alt="Submit" />
+      </form>
+    
+      <form action="" method="post" id="myform" name="form5">
+         <input type="hidden" value="5" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn5" class="btn btn-md round">this is interesting</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/interesting.png" border="0" alt="Submit" />
+      </form>
+  
+      <form action="" method="post" id="myform" name="form6">
+         <input type="hidden" value="6" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn6" class="btn btn-md round">i'm saturated</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/saturated.png" border="0" alt="Submit" />
+      </form>
+    
+      <form action="" method="post" id="myform" name="form7">
+         <input type="hidden" value="7" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn7" class="btn btn-md round">i'm lovin it</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/loving.png" border="0" alt="Submit" />
+      </form>
 
-<form action="" method="post" id="myform" name="form3">
-   <input type="hidden" value="3" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn3" class="btn btn-md round">i feel sleepy</button>
-</form>
+      <form action="" method="post" id="myform" name="form8">
+         <input type="hidden" value="8" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn8" class="btn btn-md round">i need a chai break</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/chai.png" border="0" alt="Submit" />
+      </form>
+ 
+     <form action="" method="post" id="myform" name="form9">
+         <input type="hidden" value="9" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn9" class="btn btn-md round">lost the track</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/lost_track.png" border="0" alt="Submit" />
+      </form>
+  
+     <form action="" method="post" id="myform" name="form10">
+         <input type="hidden" value="10" name="feedback"></input>
+        <!--  <button onclick="show_wait_msg()" type="submit" id="btn10" class="btn btn-md round">this is informative</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/informative.png" border="0" alt="Submit" />
+      </form>
 
-<form action="" method="post" id="myform" name="form4">
-   <input type="hidden" value="4" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn4" class="btn btn-md round">please slow down</button>
-</form>
+     <form action="" method="post" id="myform" name="form11">
+         <input type="hidden" value="11" name="feedback"></input>
+        <!--  <button onclick="show_wait_msg()" type="submit" id="btn11" class="btn btn-md round">best thing in the WORLD</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/best_thing.png" border="0" alt="Submit" />
+      </form>
 
-<form action="" method="post" id="myform" name="form5">
-   <input type="hidden" value="5" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn5" class="btn btn-md round">this is interesting</button>
-</form>
-
-<form action="" method="post" id="myform" name="form6">
-   <input type="hidden" value="6" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn6" class="btn btn-md round">i'm saturated</button>
-</form>
-
-<form action="" method="post" id="myform" name="form7">
-   <input type="hidden" value="7" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn7" class="btn btn-md round">i'm lovin it</button>
-</form>
-
-<form action="" method="post" id="myform" name="form8">
-   <input type="hidden" value="8" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn8" class="btn btn-md round">i need a chai break</button>
-</form>
-
-<form action="" method="post" id="myform" name="form9">
-   <input type="hidden" value="9" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn9" class="btn btn-md round">lost the track</button>
-</form>
-
-<form action="" method="post" id="myform" name="form10">
-   <input type="hidden" value="10" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn10" class="btn btn-md round">this is informative</button>
-</form>
-
-<form action="" method="post" id="myform" name="form11">
-   <input type="hidden" value="11" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn11" class="btn btn-md round">best thing in the WORLD</button>
-</form>
-
-<form action="" method="post" id="myform" name="form12">
-   <input type="hidden" value="12" name="feedback"></input>
-   <button onclick="show_wait_msg()" type="submit" id="btn12" class="btn btn-md round">why am i here???</button>
-</form>
-
+     <form action="" method="post" id="myform" name="form12">
+         <input type="hidden" value="12" name="feedback"></input>
+         <!-- <button onclick="show_wait_msg()" type="submit" id="btn12" class="btn btn-md round">why am i here???</button> -->
+         <input onclick="show_wait_msg()" type="image" src="images/why.png" border="0" alt="Submit" />
+      </form>
+  
 
 
 
--- results.php
+++ results.php
@@ -2,16 +2,22 @@
 <html >
   <head>
     <meta charset="UTF-8">
-    <title>Simple HTML / CSS bar-charts</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Results</title>
+    <link rel="stylesheet" href="results.css">
     </head>
 
-  <body>
+  <body onload="zoom()">
 
  <script type="text/javascript">
-/*  setTimeout(function(){
+  /*setTimeout(function(){
    window.location.reload(1);
 }, 1000);*/
+
+
+function zoom(){
+     $('#percent').addClass('zoom');
+}
+  
 </script>
 
 <?php
@@ -37,7 +43,9 @@ $arr_check = json_decode($output_check,true);
 
 <div class="container horizontal rounded">
   <div class="progress-bar horizontal">
+    This is boring...
     <div class="progress-track">
+    
       <div class="progress-fill" id="boring">
         <span><?php echo ($arr_check[0]['this is boring']*0.4);echo '%'; ?></span>
       </div>
@@ -45,7 +53,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+  Need to know more...
     <div class="progress-track">
+      
       <div class="progress-fill" id="know">
         <span><?php echo ($arr_check[0]['need to know more...']*0.4);echo '%'; ?></span>
       </div>
@@ -53,7 +63,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+   I feel sleepy...
     <div class="progress-track">
+     
       <div class="progress-fill" id="sleepy">
         <span><?php echo ($arr_check[0]['i feel sleepy']*0.4);echo '%'; ?></span>
       </div>
@@ -61,7 +73,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+     Please slow down...
     <div class="progress-track">
+   
       <div class="progress-fill" id="slow">
         <span><?php echo ($arr_check[0]['please slow down']*0.4);echo '%'; ?></span>
       </div>
@@ -69,7 +83,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+   This is interesting...
     <div class="progress-track">
+     
       <div class="progress-fill" id="interest">
         <span><?php echo ($arr_check[0]['this is interesting']*0.4);echo '%'; ?></span>
       </div>
@@ -77,14 +93,20 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+  I'm saturated...
     <div class="progress-track">
+      
       <div class="progress-fill" id="saturate">
         <span><?php echo ($arr_check[0]['im saturated']*0.4);echo '%'; ?></span>
       </div>
     </div>
   </div>
+
+
   <div class="progress-bar horizontal">
+    I'm lovin it...
     <div class="progress-track">
+    
       <div class="progress-fill" id="lovin">
         <span><?php echo ($arr_check[0]['im lovin it']*0.4);echo '%'; ?></span>
       </div>
@@ -92,7 +114,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+      Need a chai break...
     <div class="progress-track">
+  
       <div class="progress-fill" id="chai">
         <span><?php echo ($arr_check[0]['i need a chai break']*0.4);echo '%'; ?></span>
       </div>
@@ -100,14 +124,18 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+   Lost the track...
     <div class="progress-track">
+     
       <div class="progress-fill" id="track">
         <span><?php echo ($arr_check[0]['lost the track']*0.4);echo '%'; ?></span>
       </div>
     </div>
   </div>
   <div class="progress-bar horizontal">
+    This is informative...
     <div class="progress-track">
+    
       <div class="progress-fill" id="info">
         <span><?php echo ($arr_check[0]['this is informative']*0.4);echo '%'; ?></span>
       </div>
@@ -115,7 +143,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+   Best thing in the WORLD...
     <div class="progress-track">
+     
       <div class="progress-fill" id="world">
         <span><?php echo ($arr_check[0]['best thing in the WORLD']*0.4);echo '%'; ?></span>
       </div>
@@ -123,7 +153,9 @@ $arr_check = json_decode($output_check,true);
   </div>
 
   <div class="progress-bar horizontal">
+  Why am i here???
     <div class="progress-track">
+      
       <div class="progress-fill" id="why">
         <span><?php echo ($arr_check[0]['why am i here???']*0.4);echo '%'; ?></span>
       </div>
@@ -134,62 +166,84 @@ $arr_check = json_decode($output_check,true);
 <!-- Horizontal, flat -->
 
 
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
+<!--   http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js -->
+<script src='jquery1.js'></script>
         <script src="js/index.js"></script>
 
 
 <?php
 
 if($arr_check[0]['this is boring']>$arr_check[0]['need to know more...'] && $arr_check[0]['this is boring']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['this is boring']>$arr_check[0]['please slow down'] && $arr_check[0]['this is boring']>$arr_check[0]['this is interesting'] && $arr_check[0]['this is boring']>$arr_check[0]['im saturated'] && $arr_check[0]['this is boring']>$arr_check[0]['im lovin it'] && $arr_check[0]['this is boring']>$arr_check[0]['i need a chai break'] && $arr_check[0]['this is boring']>$arr_check[0]['lost the track'] && $arr_check[0]['this is boring']>$arr_check[0]['this is informative'] && $arr_check[0]['this is boring']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['this is boring']>$arr_check[0]['why am i here???']){
-        echo "THIS IS BORING";echo "<br>";
-        echo ($arr_check[0]['this is boring'] * 0.4);echo "%";
+        $message="THIS IS BORING";
+        $percent=$arr_check[0]['this is boring'] * 0.4;
+        $image="gif/boring.gif";
 }
 if($arr_check[0]['need to know more...']>$arr_check[0]['this is boring'] && $arr_check[0]['need to know more...']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['need to know more...']>$arr_check[0]['please slow down'] && $arr_check[0]['need to know more...']>$arr_check[0]['this is interesting'] && $arr_check[0]['need to know more...']>$arr_check[0]['im saturated'] && $arr_check[0]['need to know more...']>$arr_check[0]['im lovin it'] && $arr_check[0]['need to know more...']>$arr_check[0]['i need a chai break'] && $arr_check[0]['need to know more...']>$arr_check[0]['lost the track'] && $arr_check[0]['need to know more...']>$arr_check[0]['this is informative'] && $arr_check[0]['need to know more...']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['need to know more...']>$arr_check[0]['why am i here???']){
-        echo "NEED TO KNOW MORE...";echo "<br>";       
-        echo ($arr_check[0]['need to know more...'] * 0.4);echo "%";
+        $message="NEED TO KNOW MORE..."; 
+        $percent=$arr_check[0]['need to know more...'] * 0.4;      
+        $image="gif/know.gif";
 }
 if($arr_check[0]['i feel sleepy']>$arr_check[0]['need to know more...'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['this is boring'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['please slow down'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['this is interesting'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['im saturated'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['im lovin it'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['i need a chai break'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['lost the track'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['why am i here???'] && $arr_check[0]['i feel sleepy']>$arr_check[0]['this is informative']){
-        echo "I FEEL SLEEPY";echo "<br>";
-        echo ($arr_check[0]['i feel sleepy'] * 0.4);echo "%";
+        $message="I FEEL SLEEPY";
+        $percent=$arr_check[0]['i feel sleepy'] * 0.4;
+        $image="gif/sleepy.gif";
 }
 if($arr_check[0]['please slow down']>$arr_check[0]['this is informative'] && $arr_check[0]['please slow down']>$arr_check[0]['need to know more...'] && $arr_check[0]['please slow down']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['please slow down']>$arr_check[0]['this is boring'] && $arr_check[0]['please slow down']>$arr_check[0]['this is interesting'] && $arr_check[0]['please slow down']>$arr_check[0]['im saturated'] && $arr_check[0]['please slow down']>$arr_check[0]['im lovin it'] && $arr_check[0]['please slow down']>$arr_check[0]['i need a chai break'] && $arr_check[0]['please slow down']>$arr_check[0]['lost the track'] && $arr_check[0]['please slow down']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['please slow down']>$arr_check[0]['why am i here???']){
-        echo "PLEASE SLOW DOWN";echo "<br>";
-        echo ($arr_check[0]['please slow down'] * 0.4);echo "%";
+        $message="PLEASE SLOW DOWN";
+        $percent=$arr_check[0]['please slow down'] * 0.4;
+        $image="gif/slow.gif";
 }
 if($arr_check[0]['this is interesting']>$arr_check[0]['need to know more...'] && $arr_check[0]['this is interesting']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['this is interesting']>$arr_check[0]['please slow down'] && $arr_check[0]['this is interesting']>$arr_check[0]['this is boring'] && $arr_check[0]['this is interesting']>$arr_check[0]['im saturated'] && $arr_check[0]['this is interesting']>$arr_check[0]['im lovin it'] && $arr_check[0]['this is interesting']>$arr_check[0]['i need a chai break'] && $arr_check[0]['this is interesting']>$arr_check[0]['lost the track'] && $arr_check[0]['this is interesting']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['this is interesting']>$arr_check[0]['why am i here???'] && $arr_check[0]['this is interesting']>$arr_check[0]['this is informative']){
-        echo "THIS IS INTERESTING";echo "<br>";
-        echo ($arr_check[0]['this is interesting'] * 0.4);echo "%";
+        $message="THIS IS INTERESTING";
+        $percent=$arr_check[0]['this is interesting'] * 0.4;
+        $image="gif/interesting.gif";
 }
 if($arr_check[0]['im saturated']>$arr_check[0]['this is informative'] && $arr_check[0]['im saturated']>$arr_check[0]['need to know more...'] && $arr_check[0]['im saturated']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['im saturated']>$arr_check[0]['please slow down'] && $arr_check[0]['im saturated']>$arr_check[0]['this is boring'] && $arr_check[0]['im saturated']>$arr_check[0]['this is interesting'] && $arr_check[0]['im saturated']>$arr_check[0]['im lovin it'] && $arr_check[0]['im saturated']>$arr_check[0]['i need a chai break'] && $arr_check[0]['im saturated']>$arr_check[0]['lost the track'] && $arr_check[0]['im saturated']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['im saturated']>$arr_check[0]['why am i here???']){
-        echo "I'M SATURATED";echo "<br>";
-        echo ($arr_check[0]['im saturated'] * 0.4);echo "%";
+        /*$message="I'M SATURATED";
+        $percent=$arr_check[0]['im saturated'] * 0.4;
+        $image="gif/saturated.gif";*/
 }
 if($arr_check[0]['im lovin it']>$arr_check[0]['this is informative'] && $arr_check[0]['im lovin it']>$arr_check[0]['need to know more...'] && $arr_check[0]['im lovin it']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['im lovin it']>$arr_check[0]['please slow down'] && $arr_check[0]['im lovin it']>$arr_check[0]['this is interesting'] && $arr_check[0]['im lovin it']>$arr_check[0]['this is boring'] && $arr_check[0]['im lovin it']>$arr_check[0]['this is informative'] && $arr_check[0]['im lovin it']>$arr_check[0]['i need a chai break'] && $arr_check[0]['im lovin it']>$arr_check[0]['lost the track'] && $arr_check[0]['im lovin it']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['im lovin it']>$arr_check[0]['why am i here???']){
-        echo "I'M LOVIN IT";echo "<br>";
-        echo ($arr_check[0]['im lovin it'] * 0.4);echo "%";
+       /* $message="I'M LOVIN IT";
+        $percent=$arr_check[0]['im lovin it'] * 0.4;
+        $image="gif/loving.gif";*/
 }
 if($arr_check[0]['i need a chai break']>$arr_check[0]['this is informative'] && $arr_check[0]['i need a chai break']>$arr_check[0]['need to know more...'] && $arr_check[0]['i need a chai break']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['i need a chai break']>$arr_check[0]['please slow down'] && $arr_check[0]['i need a chai break']>$arr_check[0]['this is interesting'] && $arr_check[0]['i need a chai break']>$arr_check[0]['im saturated'] && $arr_check[0]['i need a chai break']>$arr_check[0]['im lovin it'] && $arr_check[0]['i need a chai break']>$arr_check[0]['this is boring'] && $arr_check[0]['i need a chai break']>$arr_check[0]['lost the track'] && $arr_check[0]['i need a chai break']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['i need a chai break']>$arr_check[0]['why am i here???']){
-        echo "I NEED A CHAI BREAK";echo "<br>";
-        echo ($arr_check[0]['i need a chai break'] * 0.4);echo "%";
+        $message="I NEED A CHAI BREAK";
+        $percent=$arr_check[0]['i need a chai break'] * 0.4;
+        $image="gif/chai.gif";
 }
 if($arr_check[0]['lost the track']>$arr_check[0]['need to know more...'] && $arr_check[0]['lost the track']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['lost the track']>$arr_check[0]['please slow down'] && $arr_check[0]['lost the track']>$arr_check[0]['this is interesting'] && $arr_check[0]['lost the track']>$arr_check[0]['im saturated'] && $arr_check[0]['lost the track']>$arr_check[0]['im lovin it'] && $arr_check[0]['lost the track']>$arr_check[0]['i need a chai break'] && $arr_check[0]['lost the track']>$arr_check[0]['this is boring'] && $arr_check[0]['lost the track']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['lost the track']>$arr_check[0]['why am i here???'] && $arr_check[0]['lost the track']>$arr_check[0]['this is informative']){
-        echo "LOST THE TRACK";echo "<br>";
-        echo ($arr_check[0]['lost the track'] * 0.4);echo "%";
+        $message="LOST THE TRACK";
+        $percent=$arr_check[0]['lost the track'] * 0.4;
+        $image="gif/lost.gif";
 }
 if($arr_check[0]['this is informative']>$arr_check[0]['this is boring'] && $arr_check[0]['this is informative']>$arr_check[0]['need to know more...'] && $arr_check[0]['this is informative']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['this is informative']>$arr_check[0]['please slow down'] && $arr_check[0]['this is informative']>$arr_check[0]['this is interesting'] && $arr_check[0]['this is informative']>$arr_check[0]['im saturated'] && $arr_check[0]['this is informative']>$arr_check[0]['im lovin it'] && $arr_check[0]['this is informative']>$arr_check[0]['i need a chai break'] && $arr_check[0]['this is informative']>$arr_check[0]['lost the track'] && $arr_check[0]['this is informative']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['this is informative']>$arr_check[0]['why am i here???']){
-        echo "THIS IS INFORMATIVE";echo "<br>";
-        echo ($arr_check[0]['this is informative'] * 0.4);echo "%";
+        $message="THIS IS INFORMATIVE";
+        $percent=$arr_check[0]['this is informative'] * 0.4;
+        $image="gif/informative.gif";
 }
 if($arr_check[0]['best thing in the WORLD']>$arr_check[0]['this is informative'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['need to know more...'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['please slow down'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['this is interesting'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['im saturated'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['im lovin it'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['i need a chai break'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['lost the track'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['this is boring'] && $arr_check[0]['best thing in the WORLD']>$arr_check[0]['why am i here???']){
-        echo "BEST THING IN THE WORLD";echo "<br>";
-        echo ($arr_check[0]['best thing in the WORLD'] * 0.4);echo "%";
+        $message="BEST THING IN THE WORLD";
+        $percent=$arr_check[0]['best thing in the WORLD'] * 0.4;
+        $image="gif/best.gif";
 }
 if($arr_check[0]['why am i here???']>$arr_check[0]['this is informative'] && $arr_check[0]['why am i here???']>$arr_check[0]['need to know more...'] && $arr_check[0]['why am i here???']>$arr_check[0]['i feel sleepy'] && $arr_check[0]['why am i here???']>$arr_check[0]['please slow down'] && $arr_check[0]['why am i here???']>$arr_check[0]['this is interesting'] && $arr_check[0]['why am i here???']>$arr_check[0]['im saturated'] && $arr_check[0]['why am i here???']>$arr_check[0]['im lovin it'] && $arr_check[0]['why am i here???']>$arr_check[0]['i need a chai break'] && $arr_check[0]['why am i here???']>$arr_check[0]['lost the track'] && $arr_check[0]['why am i here???']>$arr_check[0]['best thing in the WORLD'] && $arr_check[0]['why am i here???']>$arr_check[0]['this is boring']){
-        echo "WHY AM I HERE";echo "<br>";
-        echo ($arr_check[0]['why am i here???'] * 0.4);echo "%";
+        $message="WHY AM I HERE";
+        $percent=$arr_check[0]['why am i here???'] * 0.4;
+        $image="gif/why.gif";
 }
 ?>
+
+
+
+
+
+<h4 id="message"><?php echo $message;?></h4>
+<h5 id="percent"><?php echo $percent;echo "%"?></h5>
+<img style="margin-top:-6%" src="<?php echo $image;?>" width="20%" height="20%">
+
+
     
   </body>
 </html>
